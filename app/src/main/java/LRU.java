be4c3c0e2597e8048722a6fcc396029d3fd34334
jeselleaspiras <belleaspiras@gmail.com>
2020-11-18 09:16:55 -0800

    Initial commit

---
+++ app/src/main/java/LRU.java
@@ -0,0 +1,18 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRU {
+
+    private static final int MAX_ENTRIES = 100;
+
+    public static void main(String[] args) {
+
+        LinkedHashMap<Integer, String> hMap = new LinkedHashMap<Integer, String>() {
+                    protected boolean removeEldestEntry(Map.Entry<Integer, String> eldest)
+                    {
+                        return size() > MAX_ENTRIES;
+                    }
+                };
+
+    }
+}
